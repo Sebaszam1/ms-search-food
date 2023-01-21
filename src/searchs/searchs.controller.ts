61f0b0b1cdf159971d19e02703a7f11f8a333ede
Sebaszam1 <sebaszam1@outlook.com>
2023-01-21 09:54:28 -0500

    :memo:Fix documentation

--- src/searchs/searchs.controller.ts
+++ src/searchs/searchs.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SearchsService } from './searchs.service';
 import { CreateSearchDto } from './dto/create-search.dto';
 import { UpdateSearchDto } from './dto/update-search.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Search')
 @Controller('searchs')
@@ -10,6 +10,10 @@ export class SearchsController {
   constructor(private readonly searchsService: SearchsService) {}
 
 
+  @ApiOperation({
+    summary: 'Endpoint para buscar plato o categoria de comida',
+    description: 'Buscar algun plato por el nombre o una categoria especifica de comida,nos regresa el restaurante que tiene el plato con la mejor calificación de estrellas y además si hay mas platos en ese resturante que contengan la palabra que se escribio',
+  })
   @Get(':word')
   findOne(@Param('word') word: string) {
     return this.searchsService.searchWord(word);
--- src/searchs/searchs.service.spec.ts
+++ src/searchs/searchs.service.spec.ts
@@ -1,18 +1,103 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { SearchsService } from './searchs.service';
+import { PrismaService } from './../core/prisma/prisma.service';
+import { RestaurantsService } from 'src/restaurants/restaurants.service';
+import { MenusService } from './../menus/menus.service';
+import { CategoriesService } from './../categories/categories.service';
 
 describe('SearchsService', () => {
-  let service: SearchsService;
+  let searchsService: SearchsService;
+  let prismaService: PrismaService;
+  let restaurantsService: RestaurantsService;
+  let menusService: MenusService;
+  let categoriesService: CategoriesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [SearchsService],
-    }).compile();
-
-    service = module.get<SearchsService>(SearchsService);
+    prismaService = new PrismaService();
+    restaurantsService = new RestaurantsService(prismaService);
+    menusService = new MenusService(prismaService);
+    categoriesService = new CategoriesService(prismaService);
+    searchsService = new SearchsService(
+      restaurantsService,
+      menusService,
+      categoriesService,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('searchWord', () => {
+    it('should return a restaurant matching the search word', async () => {
+      const mockCategory = {
+        name: 'Pizza',
+        id: '5',
+        description: 'Pizza',
+        menus: [
+            {
+                menuStar: 5,
+                price: 10,
+                restaurantId: 1
+            },
+        ],
+      };
+
+      const mockMenus = [
+        {
+          name: 'Pizza Margarita',
+          menuStar: 4,
+          price: 8,
+          restaurantId: '1',
+          id: '1',
+          description: 'Pizza Margarita',
+          categoryId: '5',
+        },
+      ];
+
+      const mockRestaurant = {
+        id: '1',
+        name: 'Casa de campo',
+        cel: "5562332",
+        phone: "121223",
+        dir: "Calle 155",
+        menus: [
+          {
+            name: 'Pizza Margarita',
+            menuStar: 4,
+            price: 8,
+            restaurantId: 1,
+          },
+        ],
+        subsidiaries: [
+          {
+            id: '10',
+            name: 'Casa',
+            subStar: 2,
+            restaurantId: '1',
+          },
+        ],
+      };
+
+      jest
+        .spyOn(prismaService.category, 'findMany')
+        .mockResolvedValue([mockCategory]);
+      jest.spyOn(prismaService.menu, 'findMany').mockResolvedValue(mockMenus);
+      jest
+        .spyOn(prismaService.restaurant, 'findUnique')
+        .mockResolvedValue(mockRestaurant);
+
+      const result = await searchsService.searchWord('pizza');
+      expect(result).toEqual(mockRestaurant);
+    });
+
+    it('should throw an error if no restaurant or menu matches the search word', async () => {
+      jest.spyOn(prismaService.category, 'findMany').mockResolvedValue([]);
+      jest.spyOn(prismaService.menu, 'findMany').mockResolvedValue([]);
+
+      try {
+        await searchsService.searchWord('notexist');
+      } catch (error) {
+        expect(error.message).toEqual(
+          'No existe comida de ese tipo ni ese menu',
+        );
+        expect(error.status).toEqual(400);
+      }
+    });
   });
 });
